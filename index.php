4ac85a72e878f945511d5320f42cdbd781a51f5a
Luna Lopis <lunalopis59@gmail.com>
2023-07-05 16:26:40 +0200

    primo bonus

--- index.php
+++ index.php
@@ -1,45 +1,5 @@
 <?php
-
-    $hotels = [
-
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-
-    ];
-
+include __DIR__.'/partials/vars.php';
 ?>
 
 <!DOCTYPE html>
@@ -47,17 +7,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
     <div class="">
-    <ul>
-  <?php foreach ($hotels as $hotel) { ?>
-    <li>
-      <?php echo $hotel['name'].' - '.$hotel['description'].' - '.$hotel['parking'].' - '.$hotel['vote'].' - '.$hotel['distance_to_center']; ?>
-    </li>
-  <?php } ?>
-</ul>
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Descrizione</th>
+            <th scope="col">parcheggio</th>
+            <th scope="col">Voto</th>
+            <th scope="col">distanza dal centro</th>
+            </tr>
+        </thead>
+            <tbody>
+               <?php foreach ($hotels as $hotel) { ?>
+                    <tr>
+                    <td><?php echo $hotel['name'];?></td>
+                    <td><?php echo $hotel['description'];?></td>
+                    <td><?php echo $hotel['parking']? 'si': 'no';?></td>
+                    <td><?php echo $hotel['vote'];?></td>
+                    <td><?php echo $hotel['distance_to_center'];?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+    </table>
+
+
 
 
     </div>
